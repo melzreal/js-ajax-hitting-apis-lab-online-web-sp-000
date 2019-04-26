2ea4687afcc537c2a306ffa7ac45f1f925fc8e40
Melissa Real <melissa.real@hotmail.com>
2019-04-26 15:06:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,67 @@
 // your code here
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  let userData = document.querySelector('#username').value;
+
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${userData}/repos`);
+  req.send();
+}
+
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  let userData = document.querySelector('#username').value;
+  req.addEventListener('load', displayCommits);
+  req.open('GET', `https://api.github.com/repos/${userData}/${name}/commits`);
+  req.send();
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+
+function getBranches(el) {
+  const name =  el.dataset.repo;
+  const userData = el.dataset.username;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', `https://api.github.com/repos/${userData}/${name}/branches`);
+  req.send();
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(branch => '<li>' + branch.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
+}
