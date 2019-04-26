c372154712b9e22518d1e866b012ac0274f9beae
Melissa Real <melissa.real@hotmail.com>
2019-04-26 16:24:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -15,11 +15,10 @@ function displayRepositories() {
   const repoList = `<ul>${repos
     .map(
       r =>
-        '<li>' +
-        r.name +
+        '/' +
+        r.author.name +
         ' - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
